976bf4a3676143d6b157e5fa06a2d6106ffd8ebe
bosheng <411425403@qq.com>
2017-09-02 15:59:19 +0800

    add instance 2.1.4

--- my-scheme/chapter-2/instance/2_1_4.rkt
+++ my-scheme/chapter-2/instance/2_1_4.rkt
@@ -17,3 +17,15 @@
                 (make-interval (/ 1.0 (upper-bound y))
                                (/ 1.0 (lower-bound y)))))
 
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+
+(define (center i)
+  (let ((v1 (lower-bound i))
+        (v2 (upper-bound i)))
+    (/ (+ v1 v2) 2)))
+
+(define (width i)
+  (let ((v1 (lower-bound i))
+        (v2 (upper-bound i)))
+    (/ (- v2 v1) 2)))
