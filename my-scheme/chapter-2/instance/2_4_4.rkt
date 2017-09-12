121d971ea3b4a0916eba9111c8b866b3595851fc
bosheng <411425403@qq.com>
2017-09-12 21:45:17 +0800

    add instance 2.4.4

---
+++ my-scheme/chapter-2/instance/2_4_4.rkt
@@ -0,0 +1,19 @@
+; 2.4.4
+
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+
+(define (corner-split painter n)
+  (if (= n 0)
+      painter
+      (let ((up (up-split painter (- n 1)))
+            (right (right-split painter (- n 1))))
+        (let ((top-left (blow up up))
+              (bottom-right (below right right))
+              (corner (corner-split painter (- n 1))))
+          (beside (below painter top-left)
+                  (below bottom-right corner))))))
+
