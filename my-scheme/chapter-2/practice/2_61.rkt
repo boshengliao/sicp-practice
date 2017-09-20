470cec7ed83794c445bccc0f4cd91fd97cf0ef94
bosheng <411425403@qq.com>
2017-09-20 21:12:50 +0800

    add practice 2.61

---
+++ my-scheme/chapter-2/practice/2_61.rkt
@@ -0,0 +1,10 @@
+; 2.61
+
+(define (adjoin-set x set)
+  (cond ((null? set) (cons x set))
+        ((< x (car set)) (cons x set))
+        ((= x (car set)) set)
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(adjoin-set 2 (list 1 4))
