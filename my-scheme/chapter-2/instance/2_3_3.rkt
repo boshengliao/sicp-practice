56cc8d2e8c826320c4ec9211439e0655c0cfcea8
bosheng <411425403@qq.com>
2017-09-20 21:54:13 +0800

    add instance 2.3.3

--- my-scheme/chapter-2/instance/2_3_3.rkt
+++ my-scheme/chapter-2/instance/2_3_3.rkt
@@ -36,3 +36,36 @@
                (intersection-set-new (cdr set1) set2))
               ((> x1 x2)
                (intersection-set-new set1 (cdr set2)))))))
+
+
+(define (entry tree)
+  (car tree))
+
+(define (left-branch tree)
+  (cadr tree))
+
+(define (right-branch tree)
+  (caddr tree))
+
+(define (make-tree entry left right)
+  (list entry left right))
+
+(define (element-set?-new1 x tree)
+  (cond ((null? tree) #f)
+        ((< x (entry tree))
+         (element-set?-new1 x (left-branch tree)))
+        ((= x (entry tree)) #t)
+        (else (element?-set?-new1 x (right-branch tree)))))
+
+(define (adjoin-set-new1 x set)
+  (let ((e (entry set))
+        (l (left-branch set))
+        (r (right-branch set)))
+    (cond ((null? set) (make-tree x '() '()))
+          ((< x e) (make-tree e
+                              (adjoin-set-new1 x l)
+                              r))
+          ((= x e) set)
+          ((> x e) (make-tree e
+                              l
+                              (adjoin-set-new1 x r))))))
