67a506307d1efadd3cfc2f0dd10d46cd0f7d3378
bosheng <411425403@qq.com>
2017-09-25 21:29:43 +0800

    fix issue

--- my-scheme/chapter-2/practice/2_68.rkt
+++ my-scheme/chapter-2/practice/2_68.rkt
@@ -1,5 +1,6 @@
 ; 2.68
 
+(load "../instance/2_3_3.rkt")
 (load "../instance/2_3_4.rkt")
 
 (define sample-tree
@@ -21,11 +22,25 @@
       (append (encode-symbol (car message) tree)
               (encode (cdr message) tree))))
 
-(define (encode-symbol x tree)
+(define (encode-symbol-old x tree)
   (cond ((eq? x 'a) '(0))
         ((eq? x 'b) '(1 0))
         ((eq? x 'c) '(1 1 1))
         ((eq? x 'd) '(1 1 0))
         (else (error "wrong str" x))))
+
+(define (encode-symbol smb tree) 
+  (define (branch-correct? branch) 
+    (if (leaf? branch) 
+        (equal? smb (symbol-leaf branch)) 
+        (element-of-set? smb (symbols branch)))) 
+  
+  (let ((lb (left-branch tree)) 
+        (rb (right-branch tree))) 
+    (cond ((branch-correct? lb) 
+           (if (leaf? lb) '(0) (cons 0 (encode-symbol smb lb)))) 
+          ((branch-correct? rb) 
+           (if (leaf? rb) '(1) (cons 1 (encode-symbol smb rb)))) 
+          (else (error "bad symbol -- ENCODE-SYMBOL" bit)))))
 
 (encode '(a d a b b c a) sample-tree)
