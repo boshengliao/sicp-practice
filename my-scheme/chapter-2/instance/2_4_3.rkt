1a63038d0c0b13c8629de28be9a6d071099709c6
bosheng <411425403@qq.com>
2017-09-30 22:49:21 +0800

    fix define of put/get

--- my-scheme/chapter-2/instance/2_4_3.rkt
+++ my-scheme/chapter-2/instance/2_4_3.rkt
@@ -1,9 +1,27 @@
 ; 2.4.3
 
 ; 网上 copy 的函数定义
-(define *table* (make-hash))
-(define (get op type) (hash-ref *table* (list op type) false))
-(define (put op type val) (hash-set! *table* (list op type) val))
+(define global-array '())
+
+(define (make-entry k v) (list k v))
+(define (key entry) (car entry))
+(define (value entry) (cadr entry))
+
+(define (put op type item)
+  (define (put-helper k array)
+    (cond ((null? array) (list(make-entry k item)))
+          ((equal? (key (car array)) k) array)
+          (else (cons (car array) (put-helper k (cdr array))))))
+  (set! global-array (put-helper (list op type) global-array)))
+
+(define (get op type)
+  (define (get-helper k array)
+    (cond ((null? array) #f)
+          ((equal? (key (car array)) k) (value (car array)))
+          (else (get-helper k (cdr array)))))
+  (get-helper (list op type) global-array))
+
+  
 
 (define (install-rectangular-package)
   ;;
