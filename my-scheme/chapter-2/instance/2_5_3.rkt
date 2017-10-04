035543e87ebb51810b0b50844c01981fdc2dcc64
bosheng <411425403@qq.com>
2017-10-04 22:45:02 +0800

    fix 2.5.3

--- my-scheme/chapter-2/instance/2_5_3.rkt
+++ my-scheme/chapter-2/instance/2_5_3.rkt
@@ -83,3 +83,6 @@
 (define (make-term order coeff) (list order coeff))
 (define (order term) (car term))
 (define (coeff term) (cadr term))
+
+(define (make-polynomial var terms)
+  (get 'make 'polynomial) var terms)
