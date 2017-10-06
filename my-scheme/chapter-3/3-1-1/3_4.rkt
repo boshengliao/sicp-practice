ac6a1b10ca3b8a43469e9e1b11ffcce35346d5f4
bosheng <411425403@qq.com>
2017-10-07 00:13:02 +0800

    fix 3.4

--- my-scheme/chapter-3/3-1-1/3_4.rkt
+++ my-scheme/chapter-3/3-1-1/3_4.rkt
@@ -1,23 +1,20 @@
 ; 3.4
 
 (define (make-account balance password)
+
   (define (withdraw amount)
     (if (>= balance amount)
         (begin (set! balance (- balance amount))
                balance)
         "Insufficient funds"))
+  
   (define (deposit amount)
     (set! balance (+ balance amount))
     balance)
+  
   (define (dispatch m p)
-    (cond ((not (eq? p password))
-           (set! wrong-times (+ wrong-times 1))
-           (if (>= wrong-times 7)
-               call-the-cops
-               (lambda (x)
-                 (display wrong-times)
-                 (newline)
-                 "error password!")))
+    (cond ((not (is-correct-pw? p))
+           check-wrong-times)
           ((eq? m 'withdraw)
            (set! wrong-times 0)
            withdraw)
@@ -27,9 +24,27 @@
           (else (error
                  "Unknown request -- MAKE-ACCOUNT"
                  m))))
-  (define wrong-times 0)
+   
   (define call-the-cops
-    (lambda (x) "cops are coming!"))
+    ((lambda (x) "cops are coming!") 0))
+   
+  (define (is-correct-pw? p)
+    (if (not (eq? p password))
+        (begin (set! wrong-times (+ wrong-times 1))
+               #f)
+        (begin (set! wrong-times 0)
+               #t)))
+
+  (define wrong-times 0)
+  (define check-wrong-times
+    (lambda (x)
+      (if (>= wrong-times 7)
+          call-the-cops
+          (begin
+            (display wrong-times)
+            (newline)
+            "error password!"))))
+
   dispatch)
 
 (define a (make-account 100 '123))
