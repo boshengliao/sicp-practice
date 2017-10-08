79f6582b5edd0df9c9e5a25217e0342fb79a5e40
bosheng <411425403@qq.com>
2017-10-08 15:41:02 +0800

    add 3.7

---
+++ my-scheme/chapter-3/3-1-3/3_7.rkt
@@ -0,0 +1,61 @@
+; 3.7
+
+(define (make-account balance password)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  
+  (define identify
+    (lambda (v)
+      (if (eq? v password)
+          #t
+          #f)))
+  
+  (define set-new-account
+    (lambda (new)
+      (new-account new)))
+  
+  (define (new-account new)
+    (lambda (m p)
+      (if (eq? p new)
+          (dispatch m password)
+          "password wrong!")))
+  
+  (define (dispatch m p)
+    (cond ((eq? m 'identify) (identify p))
+          ((not (eq? p password))
+           (lambda (p) "error password!"))
+          ((eq? m 'withdraw) withdraw)
+          ((eq? m 'deposit) deposit)
+          ((eq? m 'set-new-account) set-new-account)
+          (else (error
+                 "Unknown request -- MAKE-ACCOUNT"
+                 m))))
+  
+  dispatch)
+
+(define a (make-account 100 '123))
+
+;;
+(define (make-joint account old new)
+  (if (account 'identify old)
+      ((account 'set-new-account old) new)
+      "password wrong identify!"))
+
+(a 'identify '1234)
+
+(define b (make-joint a '123 '111))
+
+(b 'identify '111)
+((b 'withdraw '111) 10)
+((a 'withdraw '123) 10)
+((a 'withdraw '123) 10)
+
+((b 'deposit '111) 30)
+((a 'withdraw '123) 0)
