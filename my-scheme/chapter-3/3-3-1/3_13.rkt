315af79811004def0eeea791512b94d88d9da6ec
bosheng <411425403@qq.com>
2017-10-10 22:40:02 +0800

    add 3.13

---
+++ my-scheme/chapter-3/3-3-1/3_13.rkt
@@ -0,0 +1,14 @@
+; 3.13
+
+(load "3_12.rkt")
+
+(define (make-cycle x)
+  (set-cdr! (last-pair x) x)
+  x)
+
+(define z (make-cycle (list 'a 'b 'c)))
+
+; 期待 (a *) (b *) (c *) (a *)...
+
+z
+(cdddr (cdddr (cddddr z)))
