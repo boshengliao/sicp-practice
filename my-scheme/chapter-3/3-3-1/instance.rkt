70d68b0fc1129eb15cbe13abb0093ea5da30900e
bosheng <411425403@qq.com>
2017-10-11 22:00:24 +0800

    join codes in instance

--- my-scheme/chapter-3/3-3-1/instance.rkt
+++ my-scheme/chapter-3/3-3-1/instance.rkt
@@ -1,7 +1,33 @@
 ; 3.3.1
 
+#|
 (define (cons x y)
   (let ((new (get-new-pair)))
     (set-car! new x)
     (set-cdr! new y)
     new))
+|#
+
+(define x (list 'a 'b))
+(define z1 (cons x x))
+
+x
+z1
+(car z1)
+(cdr z1)
+
+(define z2 (cons (list 'a 'b) (list 'a 'b)))
+
+z2
+(car z2)
+(cdr z2)
+
+(define (set-to-wow! x)
+  (set-car! (car x) 'wow)
+  x)
+
+(set-to-wow! z1)
+(set-to-wow! z2)
+
+(eq? (car z1) (cdr z1))
+(eq? (car z2) (cdr z2))
