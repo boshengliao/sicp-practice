f5e342198100246527ffd9f319b76ba32dabeb18
bosheng <411425403@qq.com>
2017-10-13 22:40:24 +0800

    fill instance

--- my-scheme/chapter-3/3-3-1/instance.rkt
+++ my-scheme/chapter-3/3-3-1/instance.rkt
@@ -31,3 +31,24 @@ z2
 
 (eq? (car z1) (cdr z1))
 (eq? (car z2) (cdr z2))
+
+
+;; 改变也就是赋值
+(define (cons- x y)
+  (define (set-x v) (set! x v))
+  (define (set-y v) (set! y v))
+  (define (dispatch m)
+    (cond ((eq? m 'car) x)
+          ((eq? m 'cdr) y)
+          ((eq? m 'set-car!) set-x)
+          ((eq? m 'set-cdr!) set-y)
+          (else (error "Undefined operation -- CONS" m))))
+  dispatch)
+
+(define ct (cons- 1 2))
+(ct 'car)
+(ct 'cdr)
+((ct 'set-car!) 5)
+((ct 'set-cdr!) 8)
+(ct 'car)
+(ct 'cdr)
