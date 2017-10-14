ec78f95d35e9f47ddb4824a4befb24c0565c09af
bosheng <411425403@qq.com>
2017-10-14 22:54:49 +0800

    add 3.22

---
+++ my-scheme/chapter-3/3-3-2/3_22.rkt
@@ -0,0 +1,61 @@
+; 3.22
+
+
+(define (make-queue)
+  (let ((queue (cons '() '())))
+    (define (front-ptr queue) (car queue))
+
+    (define (rear-ptr queue) (cdr queue))
+
+    (define (set-front-ptr! queue item)
+      (set-car! queue item))
+
+    (define (set-rear-ptr! queue item)
+      (set-cdr! queue item))
+
+    (define (empty-queue? queue)
+      (null? (front-ptr queue)))
+    
+    (define (front-queue queue)
+      (if (empty-queue? queue)
+          "FRONT called with an empty queue"
+          (car (front-ptr queue))))
+
+    (define (insert-queue! queue item)
+      (let ((new-pair (cons item '())))
+        (cond ((empty-queue? queue)
+               (set-front-ptr! queue new-pair)
+               (set-rear-ptr! queue new-pair))
+              (else
+               (set-cdr! (rear-ptr queue) new-pair)
+               (set-rear-ptr! queue new-pair)
+               (print-queue queue)))))
+
+    (define (delete-queue! queue)
+      (cond ((empty-queue? queue)
+             "DELETE called with an empty queue")
+            (else
+             (set-front-ptr! queue (cdr (front-ptr queue)))
+             (print-queue queue))))
+
+    (define (print-queue queue)
+      (front-ptr queue))
+    
+    (define (dispatch m)
+      (cond ((eq? m 'front-ptr) front-ptr)
+            ((eq? m 'rear-ptr) rear-ptr)
+            ((eq? m 'set-front-ptr!) set-front-ptr!)
+            ((eq? m 'set-rear-ptr!) set-rear-ptr!)
+            ((eq? m 'empty-queue?) empty-queue?)
+            ((eq? m 'front-queue) front-queue)
+            ((eq? m 'insert-queue!) insert-queue!)
+            ((eq? m 'delete-queue!) delete-queue!)
+            ((eq? m 'print-queue) print-queue)
+            ))
+    dispatch))
+
+(define q (make-queue))
+((q 'insert-queue!) 'a)
+((q 'insert-queue!) 'b)
+(q 'delete-queue!)
+(q 'insert-queue!)
