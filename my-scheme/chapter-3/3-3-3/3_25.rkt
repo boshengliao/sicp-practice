d476ae6fafe21b9049dea61cf0a662ac988bdea8
bosheng <411425403@qq.com>
2017-10-18 22:26:58 +0800

    add 3.25

--- my-scheme/chapter-3/3-3-3/3_25.rkt
+++ my-scheme/chapter-3/3-3-3/3_25.rkt
@@ -7,10 +7,41 @@
         (if record
             (cdr record)
             #f)))
+    
+    (define (lookup-new keys)
+      (define (iter a r)
+        (if (null? a)
+            r
+            (let ((record (assoc (car a)
+                                 (cdr local-table))))
+              (cond (record
+                     (iter (cdr a)
+                           (append r (list (cdr record)))))
+                    (else (iter (cdr a)
+                                r))
+                    ))))
+      (iter keys '()))
+    
     (define (assoc key records)
       (cond ((null? records) #f)
             ((equal? key (caar records)) (car records))
             (else (assoc key (cdr records)))))
+
+    (define (insert!-new keys value)
+      (define (iter a v)
+        (if (null? a)
+            'ok
+            (let ((record (assoc (car a)
+                                 (cdr local-table))))
+              (cond (record (set-cdr! record value))
+                    (else
+                     (set-cdr! local-table
+                      (cons (cons (car a) value)
+                            (cdr local-table)))
+                     (iter (cdr a) v))
+                    ))))
+      (iter keys value))
+    
     (define (insert! key value)
       (let ((record (assoc key (cdr local-table))))
         (if record
@@ -21,8 +52,8 @@
       'ok)
 
     (define (dispatch m)
-      (cond ((eq? m 'lookup-proc) lookup)
-            ((eq? m 'insert-proc!) insert!)
+      (cond ((eq? m 'lookup-proc) lookup-new)
+            ((eq? m 'insert-proc!) insert!-new)
             (else "Unknown operation -- TABLE")))
     dispatch))
 
@@ -30,5 +61,5 @@
 (define get (tt 'lookup-proc))
 (define put (tt 'insert-proc!))
 
-(put '1 11)
-(get '1)
+(put (list 1 2 3 4 5) 11)
+(get (list 1 4 5))
