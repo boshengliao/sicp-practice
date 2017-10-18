7650d20dc784c654937051cb49e4816c5c701ca8
bosheng <411425403@qq.com>
2017-10-18 22:05:49 +0800

    add 3.25

---
+++ my-scheme/chapter-3/3-3-3/3_25.rkt
@@ -0,0 +1,34 @@
+; 3.25
+
+(define (make-table)
+  (let ((local-table (list '*table*)))
+    (define (lookup key)
+      (let ((record (assoc key (cdr local-table))))
+        (if record
+            (cdr record)
+            #f)))
+    (define (assoc key records)
+      (cond ((null? records) #f)
+            ((equal? key (caar records)) (car records))
+            (else (assoc key (cdr records)))))
+    (define (insert! key value)
+      (let ((record (assoc key (cdr local-table))))
+        (if record
+            (set-cdr! record value)
+            (set-cdr! local-table
+                      (cons (cons key value)
+                            (cdr local-table)))))
+      'ok)
+
+    (define (dispatch m)
+      (cond ((eq? m 'lookup-proc) lookup)
+            ((eq? m 'insert-proc!) insert!)
+            (else "Unknown operation -- TABLE")))
+    dispatch))
+
+(define tt (make-table))
+(define get (tt 'lookup-proc))
+(define put (tt 'insert-proc!))
+
+(put '1 11)
+(get '1)
