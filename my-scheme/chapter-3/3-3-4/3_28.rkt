e766fb15fad6bf927bdfc57afc0e6f879d037381
bosheng <411425403@qq.com>
2017-10-21 22:48:54 +0800

    add 3.28

---
+++ my-scheme/chapter-3/3-3-4/3_28.rkt
@@ -0,0 +1,17 @@
+; 3.28
+
+(define (or-gate a1 a2 output)
+  (define (or-action-procedure)
+    (let ((new-value
+           (logical-not (get-signal a1) (get-signal a2))))
+      (after-delay and-gate-delay
+                   (lambda ()
+                     (set-signal! output new-value)))))
+  (add-action! a1 or-action-procedure)
+  (add-action! a2 or-action-procedure)
+  'ok)
+
+(define (logical-not a b)
+  (cond ((or (= a 1) (= b 1)) 1)
+        ((and (= a 0) (= b 0) 0))
+        (else "Invalid value")))
