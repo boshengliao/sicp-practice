8200e97031a63cbb561e229d3f0c98e7f16e58db
bosheng <411425403@qq.com>
2017-11-02 22:46:06 +0800

    add 3.4.2

---
+++ my-scheme/chapter-3/3-4-2/instance.rkt
@@ -0,0 +1,21 @@
+; 3.4.2
+
+;; 串行化
+(define (make-account balance)
+  (define (withdraw amount)
+    (if (>= balance amount)
+        (begin (set! balance (- balance amount))
+               balance)
+        "Insufficient funds"))
+  (define (deposit amount)
+    (set! balance (+ balance amount))
+    balance)
+  (let ((protected (make-serializer)))
+    (define (dispatch m)
+      (cond ((eq? m 'withdraw) (protected withdraw))
+            ((eq? m 'deposit) (protected deposit))
+            ((eq? m 'balance) balance)
+            (else (display "Unknown request -- MAKE-ACCOUNT ")
+                  (display m)
+                  (newline)))))
+  dispatch)
