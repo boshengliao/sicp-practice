b9a8e9c69aaff726378f1428c85bc573d1676eaa
bosheng <411425403@qq.com>
2017-11-21 22:32:22 +0800

    add some comments

--- my-scheme/chapter-3/3-5-1/3_51.rkt
+++ my-scheme/chapter-3/3-5-1/3_51.rkt
@@ -3,13 +3,41 @@
 (load "instance.rkt")
 
 (define (show x)
+  (display "A")
   (display-line x)
-  (newline))
+  (newline)
+  x)
+#||#
+(define (a proc)
+  (display "a")
+  (lambda () proc))
+
+(a (+ 1 2))
+
+(define (b s n)
+  (if (= n 0)
+      (car s)
+      (cons (car s)
+            (a (b (cdr s) (- n 1))))))
+
+(b (list 1 2 3 4) 3)
+(define c (b (list 1 2 3 4) 3))
+
+(car c)
+((cdr c))
+
+
+
 
 (define x (stream-map show
                       (stream-enumerate-interval 0 10)))
 
-;(stream-ref x 5)
-(display "aaa")
+(stream-ref x 5)
+
+;(display "aaa")
 (stream-ref x 7)
-(display "aaa")
+;(display "aaa")
+
+#|
+不知道为什么, stream 信息没有得到保存, 每次都是从头开始读取
+|#
